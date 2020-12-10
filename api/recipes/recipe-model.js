615ca07739a66833a0047ea7ec2d5e8183f59aee
Olivia Toolson <livytoolson@gmail.com>
2020-12-10 14:56:30 -0800

    MVP / STRECH

--- api/recipes/recipe-model.js
+++ api/recipes/recipe-model.js
@@ -11,20 +11,6 @@ function getRecipes() {
 }
 
 function getShoppingList(id) {
-// select 
-//     ingredients.ingredients,
-//     step_ingredients.quantity,
-//     units.unit_type 
-// from recipes 
-// join steps 
-//    on steps.recipe_id = recipes.recipe_id
-// join step_ingredients 
-//    on step_ingredients.step_id = steps.step_id
-// join ingredients 
-//    on step_ingredients.ingredient_id = ingredients.ingredient_id
-// join units
-//    on units.unit_id = step_ingredients.unit_id
-// where recipes.recipe_id = 1;
     return db('recipes')
     .select('ingredients.ingredients', 'step_ingredients.quantity', 'units.unit_type')
     .join('steps', 'steps.recipe_id', 'recipes.recipe_id')
@@ -35,14 +21,6 @@ function getShoppingList(id) {
 }
 
 function getInstructions(id) {
-// select 
-//     r.name,
-//     s.instruction,
-//     s.step_number
-// from recipes r
-// join steps s
-//     on s.recipe_id = r.recipe_id
-// where r.recipe_id = 1;
     return db('recipes')
     .select('recipes.name', 'steps.instruction', 'steps.step_number')
     .join('steps', 'steps.recipe_id', 'recipes.recipe_id')
